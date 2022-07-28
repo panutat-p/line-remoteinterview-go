d2b88bada2c5f407fc618ea75d120b09cf42e9b3
panutat-p <pp.panutat@gmail.com>
2022-07-29 03:18:55 +0700

    GET /, set timeout

--- main.go
+++ main.go
@@ -5,14 +5,20 @@ import (
 	"github.com/panutat-p/line-remoteinterview-go/health"
 	"log"
 	"net/http"
+	"time"
 )
 
 const PORT = 8080
 
 func main() {
-	http.HandleFunc("/", health.CheckHealth)
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", PORT),
+		Handler:      http.HandlerFunc(health.CheckHealth),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 	fmt.Println("🟧 handler GET /")
-	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
